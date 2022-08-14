a187bc6af66c53f9c97cda754aec1bb66454ba73
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2022-08-15 07:40:14 +0900

    solve ex3

--- golang/exercise-3/exercise-maps.go
+++ golang/exercise-3/exercise-maps.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"strings"
+
 	"golang.org/x/tour/wc"
 )
 
 func WordCount(s string) map[string]int {
-	return map[string]int{"x": 1}
+	m := make(map[string]int)
+	for _, word := range strings.Fields(s) {
+		m[word]++
+	}
+	return m
 }
 
 func main() {
