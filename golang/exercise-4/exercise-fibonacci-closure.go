ce18c8a28d82c31b9cff83619a88e2d504065edc
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2022-08-15 07:46:48 +0900

    solve ex4

--- golang/exercise-4/exercise-fibonacci-closure.go
+++ golang/exercise-4/exercise-fibonacci-closure.go
@@ -5,6 +5,11 @@ import "fmt"
 // fibonacci is a function that returns
 // a function that returns an int.
 func fibonacci() func() int {
+	var a1, a2 int = 0, 1
+	return func() int {
+		defer func() { a1, a2 = a2, a1+a2 }()
+		return a1
+	}
 }
 
 func main() {
