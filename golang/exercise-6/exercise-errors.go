e20e6bdd7aedc9032141223f563263b8b72815b8
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2022-08-15 10:47:01 +0900

    solve ex06

--- golang/exercise-6/exercise-errors.go
+++ golang/exercise-6/exercise-errors.go
@@ -2,10 +2,29 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("cannot Sqrt negative number: %v", float64(e))
+}
+
 func Sqrt(x float64) (float64, error) {
-	return 0, nil
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x)
+	}
+
+	z := x
+	newz := z - (z*z-x)/(2*z)
+
+	for math.Abs(newz-z)/((newz+z)/2) > 1e-6 {
+		z = newz
+		newz = z - (z*z-x)/(2*z)
+		fmt.Printf("guess: %v\n", z)
+	}
+	return z, nil
 }
 
 func main() {
