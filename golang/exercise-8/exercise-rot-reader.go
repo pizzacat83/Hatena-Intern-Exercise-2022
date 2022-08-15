b5ee9426469af049d3f3a2727129ca2210516366
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2022-08-15 18:45:29 +0900

    solve ex8

--- golang/exercise-8/exercise-rot-reader.go
+++ golang/exercise-8/exercise-rot-reader.go
@@ -10,6 +10,28 @@ type rot13Reader struct {
 	r io.Reader
 }
 
+func rot13(c byte) byte {
+	switch {
+	case 'a' <= c && c <= 'z':
+		return (c-'a'+13)%26 + 'a'
+	case 'A' <= c && c <= 'Z':
+		return (c-'A'+13)%26 + 'A'
+	default:
+		return c
+	}
+}
+
+func (r rot13Reader) Read(buf []byte) (int, error) {
+	n, err := r.r.Read(buf)
+	if err != nil {
+		return 0, err
+	}
+	for i := 0; i < n; i++ {
+		buf[i] = rot13(buf[i])
+	}
+	return n, nil
+}
+
 func main() {
 	s := strings.NewReader("Lbh penpxrq gur pbqr!")
 	r := rot13Reader{s}
