669616dc5725652f1258e4cd29ae85afb7108f71
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2022-08-20 16:38:05 +0900

    implement simple fizzbuzz

--- typescript/src/fizzbuzz.ts
+++ typescript/src/fizzbuzz.ts
@@ -4,5 +4,26 @@ export function initializeFizzBuzz(
 ): void {
   console.debug("initializeFizzBuzz", list, button);
 
-  // Implement Me!
+  let i = 1;
+
+  button.addEventListener("click", () => {
+    const text = fizzbuzz(i);
+    const li = document.createElement("li");
+    // innerText works in browser but not in test (jsdom) https://github.com/jsdom/jsdom/issues/1245
+    li.textContent = text;
+    list.appendChild(li);
+    i += 1;
+  });
+}
+
+const fizzbuzz = (i: number): string => {
+  if (i % 3 == 0 && i % 5 == 0) {
+    return "FizzBuzz";
+  } else if (i % 3 == 0) {
+    return "Fizz";
+  } else if (i % 5 == 0) {
+    return "Buzz";
+  } else {
+    return i.toString();
+  }
 }
