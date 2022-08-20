099c311a14d189ae0f1529392b24972c1bd28ae6
pizzacat83 <17941141+pizzacat83@users.noreply.github.com>
2022-08-20 18:10:53 +0900

    reimplement fizzbuzz with generator

--- typescript/src/fizzbuzz.ts
+++ typescript/src/fizzbuzz.ts
@@ -4,10 +4,14 @@ export function initializeFizzBuzz(
 ): void {
   console.debug("initializeFizzBuzz", list, button);
 
-  let counter = new Counter();
+  let counter = create_counter();
 
   button.addEventListener("click", () => {
-    const text = fizzbuzz(counter.next());
+    const item = counter.next();
+    // There's no way to express a type of infinite generator in Typescript
+    // and there's a reason: https://stackoverflow.com/questions/72167179/how-to-specify-type-of-an-infinite-generator
+    if (item.done) throw Error("unreachable");
+    const text = fizzbuzz(item.value);
     const li = document.createElement("li");
     // innerText works in browser but not in test (jsdom) https://github.com/jsdom/jsdom/issues/1245
     li.textContent = text;
@@ -15,11 +19,11 @@ export function initializeFizzBuzz(
   });
 }
 
-class Counter {
-  #next = 1;
-
-  next(): number {
-    return this.#next++;
+function* create_counter() {
+  let i = 1;
+  while (true) {
+    yield i;
+    i += 1;
   }
 }
 
